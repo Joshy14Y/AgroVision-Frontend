d57d2d4e045102cc75d91e28f6a1dfb15502ed9a
Joshy14Y <joshygamer14y@gmail.com>
2026-02-13 17:20:35 -0600

    feat(inference-result): refactor inference result component and update styles for improved layout and functionality

--- src/features/fruit-freshness/pages/inference-page/inference-page.ts
+++ src/features/fruit-freshness/pages/inference-page/inference-page.ts
@@ -6,4 +6,12 @@ import { InferenceResultComponent } from '@features/fruit-freshness/components/i
   imports: [InferenceResultComponent],
   templateUrl: './inference-page.html',
 })
-export class InferencePage {}
+export class InferencePage {
+  analysis = {
+    class: 'Manzana',
+    confidence: 0.90,
+    time: 120,
+    model: 'v3.2_ResNet152',
+    batch_id: '#TEST-001',
+  };
+}
